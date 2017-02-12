067862a4f06f585c294daade68fe0f8b0b532a03
zr023 <laslo023@gmail.com>
2017-02-12 12:34:00 +1000

    Added fibonacci & words controllers

---
+++ BrightSpark/Controllers/FibonacciController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BrightSpark.Controllers
+{
+    public class FibonacciController : ApiController
+    {
+        // GET: api/Fibonacci
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Fibonacci/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Fibonacci
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Fibonacci/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Fibonacci/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BrightSpark/Controllers/WordsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BrightSpark.Controllers
+{
+    public class WordsController : ApiController
+    {
+        // GET: api/Words
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Words/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Words
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Words/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Words/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
